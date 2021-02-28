c97bee7c826d0b8d1b69581ef20a7b3223a97d9f
Lionel Raseemela <lraseemela@gmail.com>
2021-02-28 19:49:35 +0200

    added second test to check the range

---
+++ spec/support/score_spec.js
@@ -0,0 +1,11 @@
+const { scorePredictor } = require(`../../src/score.js`)
+
+describe('scorePredictor', () => {
+    it(`should be a number`, () => {
+        expect(typeof(scorePredictor())).toBe('number')
+    });
+
+    it(`should be in a range of 0.0 to 1.0`, () => {
+        expect(scorePredictor()).toBeLessThanOrEqual(1.0)
+    });
+})
